6cb578124d1546818119cc13f410706a1864859a
andrewsil1 <andrewsi@microsoft.com>
2017-09-19 15:30:13 -0700

    Revert private changes to tim.v, since it's no longer needed for the Xilinx Nexys4 implementation.

--- HDL/tim.v
+++ HDL/tim.v
@@ -21,78 +21,48 @@ the Propeller 1 Design.  If not, see <http://www.gnu.org/licenses/>.
 
 module tim
 (
-    input           clock_160,  // external clock, nominally 160MHz
-    input           pllX16,     // 1/2 external clock, i.e. 80Mhz
-    input           pllX8,      // 40Mhz
-    input           pllX4,      // 20Mhz
-    input           pllX2,      // 10Mhz
-    input           pllX1,      //  5Mhz
+    input           clk,        // clock, nominally 160MHz
     input           res,        // reset
     input [6:0]     cfg,        // 7LSBs of CLK register structure 
     
-    output          clk_pll,    // "Fake PLL" clock, 2x the cog clock.
-    output          clk_cog     // Cog clock out, from selector logic below
+    output          clk_pll,    // "PLL" clock, twice the rate of clk_cog
+    output          clk_cog     // cog clock
 );
 
-reg [6:0]   cfgx = 7'b0;
-reg [12:0]  divide = 13'b0;
-
-wire pllX8_or_4, pllX4_or_2, pllX2_or_1, pllX1_or_rcslow;
+reg [6:0]   cfgx;
+reg [12:0]  divide;
 
 wire[4:0] clksel = {cfgx[6:5], cfgx[2:0]};  // convenience, skipping the OSCM1 and OSCM0 signals
 
-always @ (posedge clock_160)
+assign clk_pll = (clksel == 5'b11111)       // If set to PLL16X
+                    ? clk                   // PLL is the full clock rate
+                    : divide[11];           // Otherwise, it is twice the rate of clk_cog
+
+assign clk_cog = divide[12];                // Half the rate of clk_pll
+
+always @ (posedge clk)
 begin
     cfgx <= cfg;
 end
 
-//   BUFGMUX_CTRL BUFGMUX_CTRL_clkpll (
-//      .O(clk_pll),              // 1-bit output: PLL clock output
-//      .I0(cogclk_x2),           // 1-bit input: CogClk x 2 when clock mode is something other than PLLX16
-//      .I1(clock_160),           // 1-bit input: Use clock_160 when set to PLLx16 mode
-//      .S(clksel == 5'b11111)    // 1-bit input: (Is clock mode PLLx16?)
-//   );
-   assign clk_pll = clock_160;    // TODO: Find some way to select the right 2x clock for the fake PLL without overusing BUFGMUX_CTRL's
-      
-   BUFGMUX_CTRL BUFGMUX_CTRL_cogclk (
-      .O(clk_cog),
-      .I0(pllX8_or_4),
-      .I1(pllX16),
-      .S(clksel == 5'b11111)      // Select PLLX16 if true, otherwise lower
-   );
-   
-   BUFGMUX_CTRL BUFGMUX_CTRL_medpll (
-      .O(pllX8_or_4),
-      .I0(pllX4_or_2),
-      .I1(pllX8),
-      .S(clksel == 5'b11110) // Select PLLX8 when true, otherwise lower
-   ); 
-
-   BUFGMUX_CTRL BUFGMUX_CTRL_lowpll (
-      .O(pllX4_or_2),
-      .I0(pllX2_or_1),
-      .I1(pllX4),
-      .S(clksel == 5'b11101) // Select PLLX4 when true, otherwise lower
-   );
-   
-   BUFGMUX_CTRL BUFGMUX_CTRL_lastpll (
-      .O(pllX2_or_1),
-      .I0(pllX1_or_rcslow),
-      .I1(pllX2),
-      .S(clksel == 5'b11100 || clksel[2:0] == 3'b000) // Select PLLX2 when true, otherwise lower
-   );
-      
-   BUFGMUX_CTRL BUFGMUX_CTRL_rcslow (
-      .O(pllX1_or_rcslow),
-      .I0(divide[12]),
-      .I1(pllX1),
-      .S(clksel == 5'b11011 || clksel == 5'b01010) // Select PLLX1 or XINPUT when true, otherwise RCSLOW
-   );      
-
-always @ (posedge clock_160)
+always @ (posedge clk)
 begin
     divide <= divide + 
-    {res, 11'b00000000000, !res}; //13 bit counter at 160Mhz results in divide[12] toggling at ~19.5Khz for RCSLOW
+    {
+         clksel == 5'b11111 || res,                                                     // PLL16X or reset
+         clksel == 5'b11110 && !res,                                                    // PLL8X
+         clksel == 5'b11101 && !res,                                                    // PLL4X
+        (clksel == 5'b11100 || clksel[2:0] == 3'b000) && !res,                          // PLL2X or RCFAST
+        (clksel == 5'b11011 || (clksel[4] == 1'b1 && clksel[2:0] == 3'b010)) && !res,   // PLL1X or XINPUT
+         1'b0,
+         1'b0,
+         1'b0,
+         1'b0,
+         1'b0,
+         1'b0,
+         1'b0,
+         clksel[2:0] == 3'b001 && !res                                                  // RCSLOW
+        };
 end
 
 endmodule
